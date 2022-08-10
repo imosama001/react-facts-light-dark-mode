782326d2539353628870a1454837f6eda4dc8622
mohd osama raza <imosama001@gmail.com>
2022-08-10 10:10:58 +0530

    initial commit : a web page to toggle between light to dark mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+// import dark from "./images/react-dark.png"
 function App() {
+  const [darkMode,setDarkMode]=useState(false);
+
+  function toggleDarkMode(){
+    setDarkMode(prevMode=>!prevMode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <img src={dark} alt ="logo dark"/>
+    <div className='container'>
+
+      <Navbar  darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      <Main darkMode={darkMode}/>
+      
     </div>
   );
 }
